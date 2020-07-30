ee541521817d0c77cc67dede54cd6664f986fff6
Drason Chow <emchow@iu.edu>
2020-07-30 00:28:14 -0400

    started expert SDK, binding the enable and disable expert SDK functions

--- forcedimension/dhd/bindings/expert.py
+++ forcedimension/dhd/bindings/expert.py
@@ -6,3 +6,36 @@
 .. moduleauthor:: Drason Chow <drasonchow@gmail.com>
 """
 
+
+from typing import Tuple, List, Union, Optional, cast
+
+from ctypes import (
+    c_int, c_uint, c_bool, c_byte, c_ushort, c_char_p, c_double
+)
+
+from ctypes import byref, POINTER
+
+from forcedimension.dhd.bindings import _libdhd
+
+_libdhd.dhdEnableExpertMode.argtypes = []
+_libdhd.dhdEnableExpertMode.restype = c_int
+def enableExpertMode() -> int: # NOQA
+    """
+    Enable expert mode.
+
+    :rtype: int
+    :returns: 0 on success, -1 otherwise.
+    """
+    return _libdhd.dhdEnableExpertMode()
+
+
+_libdhd.dhdDisableExpertMode.argtypes = []
+_libdhd.dhdDisableExpertMode.restype = c_int
+def disableExpertMode() -> int: # NOQA
+    """
+    Enable expert mode.
+
+    :rtype: int
+    :returns: 0 on success, -1 otherwise.
+    """
+    return _libdhd.dhdDisableExpertMode()
