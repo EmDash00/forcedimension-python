08a07bf6c72f14a2696437a5d7782a8a7e9d08b6
Drason Chow <emchow@iu.edu>
2020-07-31 11:55:19 -0400

    finished deltaEncoderToPosition

--- forcedimension/dhd/bindings/expert.py
+++ forcedimension/dhd/bindings/expert.py
@@ -400,3 +400,53 @@ def setGripperMotor(output: int, ID: int = -1) -> int: # NOQA
 
     """
     return _libdhd.dhdSetWristMotor(output, ID)
+
+
+_libdhd.dhdDeltaEncoderToPosition.argtypes = [
+    c_int,
+    c_int,
+    c_int,
+    POINTER(c_double),
+    POINTER(c_double),
+    POINTER(c_double),
+    c_byte
+]
+_libdhd.dhdDeltaEncoderToPosition.restype = c_int
+def deltaEncoderToPosition(enc: Tuple[int, int, int], # NOQA
+                           ID: int = -1) -> Tuple[List[float], int]:
+    """
+    This routine computes and returns the position of the end-effector for a
+    given set of encoder readings.
+
+    :param Tuple[int, int, int] enc: tuple of (enc0, enc1, enc2) where enc0,
+    enc1, and enc2 coresspond to encoder readings on axis 0, 1, and 2,
+    respectively.
+
+    :param int ID: [default=-1] device ID (see multiple devices section
+    for details)
+
+    :raises ValueError: if any member of output is not implicitly convertible
+    to C ushort.
+
+    :raises ValueError: if ID is not implicitly convertible to C char
+
+    :rtype: int
+    :returns: tuple of ([px, py, pz], err) where err is 0 on success,
+    -1 otherwise and [px, py, pz] is a list of floats corresponding to the
+    DELTA end-effector position on the X, Y, and Z axes, respectively in [m].
+    """
+
+    px = c_double()
+    py = c_double()
+    pz = c_double()
+
+    return ([px.value, py.value, pz.value],
+            _libdhd.dhdDeltaEncoderToPosition(
+                    enc[0],
+                    enc[1],
+                    enc[2],
+                    byref(px),
+                    byref(py),
+                    byref(pz)
+                )
+            )
