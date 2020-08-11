c5621bee8d7e2ae7fb5fe47a0f56c0980908680b
Drason Chow <emchow@iu.edu>
2020-08-11 14:44:52 -0400

    added getDeviceID(), which was left out

--- forcedimension/dhd/bindings/__init__.py
+++ forcedimension/dhd/bindings/__init__.py
@@ -108,6 +108,19 @@ def setDevice(ID: int = -1) -> int:  # NOQA
     return _libdhd.setDevice(ID)
 
 
+_libdhd.dhdGetDeviceID.argtypes = []
+_libdhd.dhdGetDeviceID.restype = c_int
+def getDeviceID() -> int: # NOQA
+    """
+    Get the ID of the current default device.
+
+    :rtype: int
+    :returns: the ID of the current default device
+    """
+
+    return _libdhd.dhdGetDeviceID()
+
+
 _libdhd.dhdGetSerialNumber.argtypes = [POINTER(c_ushort), c_byte]
 _libdhd.dhdGetSerialNumber.restype = c_int
 def getSerialNumber(ID: int = -1) -> Tuple[int, int]:  # NOQA
