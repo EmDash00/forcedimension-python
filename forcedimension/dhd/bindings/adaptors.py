b80e6cc9b29e9f9587057e2fb2dd134e71e97f3c
Drason Chow <emchow@iu.edu>
2020-08-21 21:03:52 -0400

    fixed some serious issues with exception resolution, instantiation, and inheritance so that it's now easier to convert between errno and them.

--- forcedimension/dhd/bindings/adaptors.py
+++ forcedimension/dhd/bindings/adaptors.py
@@ -83,84 +83,187 @@ class DOFTuple(NamedTuple):
 
 
 class DHDError(Exception):
-    def __init__(self, msg="Undocumented error.", **kwargs):
-        return super().__init__(msg)
-
-
-class DHDErrorCom(IOError):
-    def __init__(self, **kwargs):
-        return super().__init__("Communication error between the HapticDevice "
-                                "and the host computer.")
-
-
-class DHDErrorDHCBusy(IOError):
-    def __init__(self, **kwargs):
-        return super().__init__("The device controller is busy and cannot "
-                                "perform the required task")
-
-
-class DHDErrorNoDriverFound(IOError):
-    def __init__(self, ID: Optional[int] = None, **kwargs):
-        if (ID is not None):
-            specification = " for device ID {} ".format(ID)
+    def __init__(self, msg: Optional[str] = "Undocumented error."):
+        if msg is not None:
+            return super().__init__(msg)
         else:
-            specification = " "
-
-        return super().__init__("A required device driver{}is not installed, "
-                                "please refer to your user manual's "
-                                "instalation section".format(specification))
+            return super().__init__()
 
 
-class DHDErrorNoDeviceFound(IOError):
-    def __init__(self, **kwargs):
-        return super().__init__("No compatible force dimension device was "
-                                "found. ")
-
-
-class DHDErrorNotAvailable(Exception):
+class DHDFeatureError(DHDError):
     def __init__(
         self,
+        *,
+        reason: str,
+        ID: Optional[int] = None,
+        feature: Optional[Callable[[Any], Any]]
+    ):
+        err_msg = "{} is not available {} because {}."
+        feature_seg = "A particular feature" if feature is None else feature
+        id_seg = "" if ID is None else "on device {}".format(ID)
+
+        return super().__init__(err_msg.format(feature_seg, id_seg, reason))
+
+
+class DHDErrorExpertModeDisabled(DHDFeatureError):
+    def __init__(
+        self,
+        *,
+        feature: Optional[Callable[[Any], Any]] = None,
+        **kwargs
+    ):
+        return super().__init__(
+            reason="expert mode is disabled",
+            ID=None,
+            feature=feature,
+        )
+
+
+class DHDErrorFeatureNotAvailable(DHDFeatureError):
+    def __init__(
+        self,
+        *,
         feature: Optional[Callable[[Any], Any]],
         ID: Optional[int] = None,
         **kwargs
     ):
 
-        if feature is not None:
-            feature_str = "{}".format(feature)
-        else:
-            feature_str = "The requested feature"
-
-        if ID is not None:
-            spec = "device ID {}".format(ID)
-        else:
-            spec = "the ."
-
-        return super().__init__("{} is not avaiable for "
-                                "{}.".format(feature_str, spec))
+        return super().__init__(
+            reason="it is not supported on this device",
+            ID=ID,
+            feature=feature
+        )
 
 
-class DHDErrorTimeout(IOError):
+class DHDErrorFeatureNotEnabled(DHDFeatureError):
     def __init__(
         self,
-        operation: Optional[Callable[[Any], Any]] = None,
+        *,
+        feature: Optional[Callable[[Any], Any]] = None,
         ID: Optional[int] = None,
         **kwargs
     ):
-        if (operation is not None):
-            op_str = "{}".format(operation)
-        else:
-            op_str = "The operation"
 
-        if (ID is not None):
+        return super().__init__(
+            reason="it was previously disabled for this device",
+            ID=ID,
+            feature=feature
+        )
+
+
+class DHDErrorDeviceNotReady(DHDFeatureError):
+    def __init__(
+        self,
+        *,
+        feature: Optional[Callable[[Any], Any]],
+        ID: Optional[int] = None,
+        **kwargs
+    ):
+        return super().__init__(
+            reason="the device isn't ready to proccess a new command",
+            feature=feature,
+            ID=ID
+        )
+
+
+class DHDErrorRedundantFail(DHDError):
+    def __init__(self, *, ID: Optional[int] = None, **kwargs):
+        if ID is not None:
             spec = " on device ID {}".format(ID)
         else:
             spec = ""
 
-        return super().__init__("{} has timed out{}.".format(op_str, spec))
+        super().__init__("The redundant encoder integrity "
+                         "test failed{}.".format(spec))
 
 
-class DHDErrorGeometry(IOError):
-    def __init__(self, ID: Optional[int] = None, **kwargs):
+class DHDIOError(DHDError, OSError):
+    def __init__(
+        self,
+        *,
+        err: str,
+        ID: Optional[int] = None,
+        op: Optional[Callable[[Any], Any]] = None
+    ):
+        err_msg = "{}{}{}."
+        op_seg = "" if op is None else "{} failed.".format(op)
+        id_seg = "" if ID is None else " occured on device {}".format(ID)
+
+        return super().__init__(err_msg.format(op_seg, err, id_seg))
+
+
+class DHDErrorTimeout(DHDIOError):
+    def __init__(
+        self,
+        *,
+        op: Optional[Callable[[Any], Any]] = None,
+        ID: Optional[int] = None,
+        **kwargs
+    ):
+
+        return super().__init__(
+            err="timeout",
+            ID=ID,
+            op=op
+        )
+
+
+class DHDErrorCom(DHDIOError):
+    def __init__(
+        self,
+        *,
+        err: str,
+        ID: Optional[int] = None,
+        **kwargs
+    ):
+        return super().__init__(
+            err="a communication error between the host and the HapticDevice",
+            ID=ID
+        )
+
+
+class DHDErrorDHCBusy(DHDIOError):
+    def __init__(self, *, ID: Optional[int] = None, **kwargs):
+        return super().__init__(
+            err="The device controller is busy.",
+            ID=ID
+        )
+
+
+class DHDErrorNoDeviceFound(DHDIOError):
+    def __init__(self, **kwargs):
+        return super().__init__(
+            err="No compatible ForceDimension devices found."
+        )
+
+
+class DHDErrorDeviceInUse(DHDIOError):
+    def __init__(self, *, ID: Optional[int] = None, **kwargs):
+        super().__init__(
+            err="Open error (because the device is already in use)",
+            ID=ID
+        )
+
+
+class DHDErrorNoDriverFound(DHDIOError):
+    def __init__(self, *, ID: Optional[int] = None, **kwargs):
+        return super().__init__(
+            err="A required driver is not installed (see device manual for"
+                "details)",
+            ID=ID)
+
+
+class DHDErrorConfiguration(DHDIOError):
+    def __init__(self, *, ID: Optional[int] = None, **kwargs):
+        super().__init__(
+            err="IO error trying to read/write calibration data from "
+                "device memory",
+            ID=ID
+        )
+
+
+class DHDErrorGeometry(DHDError):
+    def __init__(self, ID: Optional[int] = None):
 
         if (ID is not None):
             spec = "device ID {}".format(ID)
@@ -171,90 +274,6 @@ class DHDErrorGeometry(IOError):
                                 "geometric model".format(spec))
 
 
-class DHDErrorExpertModeDisabled(Exception):
-    def __init__(self,
-                 feature: Optional[Callable[[Any], Any]] = None,
-                 **kwargs):
-        return super().__init__("{} is not available because expert mode is "
-                                "disabled.".format(feature))
-
-
-class DHDErrorDeviceNotReady(Exception):
-    def __init__(
-        self,
-        cmd: Optional[Callable[[Any], Any]],
-        ID: Optional[int] = None,
-        **kwargs
-    ):
-
-        if cmd is not None:
-            cmd_str = "The {} feature".format(cmd)
-        else:
-            cmd_str = "The requested command"
-
-        if ID is not None:
-            spec = "device ID {} ".format(ID)
-        else:
-            spec = "the device"
-
-        return super().__init__("{} is not ready to process on "
-                                "{}.".format(cmd_str, spec))
-
-
-class DHDErrorConfiguration(IOError):
-    def __init__(self, ID: Optional[int] = None, **kwargs):
-        if ID is not None:
-            spec = "device ID {}'s ".format(ID)
-        else:
-            spec = "the device"
-
-        super().__init__("There was an error trying to read/write the "
-                         "calibration data into {} memory".format(spec))
-
-
-class DHDErrorRedundantFail(Exception):
-    def __init__(self, ID: Optional[int] = None, **kwargs):
-        if ID is not None:
-            spec = " on device ID {}".format(ID)
-        else:
-            spec = ""
-
-        super().__init__("The redundant encoder integrity "
-                         "test failed{}.".format(spec))
-
-
-class DHDErrorNotEnabled(Exception):
-    def __init__(
-        self,
-        cmd: Optional[Callable[[Any], Any]],
-        ID: Optional[int] = None,
-        **kwargs
-    ):
-
-        if cmd is not None:
-            cmd_str = "The {} feature".format(cmd)
-        else:
-            cmd_str = "A particular feature"
-
-        if ID is not None:
-            spec = "device ID {} ".format(ID)
-        else:
-            spec = "the device."
-
-        return super().__init__("{} is not enabled on "
-                                "{}.".format(cmd_str, spec))
-
-
-class DHDErrorDeviceInUse(IOError):
-    def __init__(self, ID: Optional[int] = None, **kwargs):
-        if ID is not None:
-            spec = "Device ID {}".format(ID)
-        else:
-            spec = "The device"
-
-        super().__init__("{} is already in use".format(spec))
-
-
 _error = [
     None,
     DHDError,
@@ -262,7 +281,7 @@ _error = [
     DHDErrorDHCBusy,
     DHDErrorNoDriverFound,
     DHDErrorNoDeviceFound,
-    DHDErrorNotAvailable,
+    DHDErrorFeatureNotAvailable,
     DHDErrorTimeout,
     DHDErrorGeometry,
     DHDErrorExpertModeDisabled,
@@ -273,7 +292,7 @@ _error = [
     DHDErrorConfiguration,
     ValueError,
     DHDErrorRedundantFail,
-    DHDErrorNotEnabled,
+    DHDErrorFeatureNotEnabled,
     DHDErrorDeviceInUse
 ]
 
