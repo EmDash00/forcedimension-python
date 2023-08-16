2900487310cf0609fbc3e9cbbe76b7999a980ee5
Ember Chow <emberchow.businiess@iu.edu>
2023-08-16 14:01:03 -0700

    Add config and info BaseModels.

--- forcedimension/containers.py
+++ forcedimension/containers.py
@@ -1,12 +1,20 @@
 import ctypes
+import dataclasses
 from array import array
 from ctypes import c_double, c_int, c_ushort
-from typing import Iterable, List, NamedTuple, Optional, Tuple
+from math import nan
+from typing import Iterable, List, Literal, NamedTuple, Optional, Tuple
 
-from forcedimension.dhd.constants import MAX_DOF
+import pydantic
+
+from forcedimension.dhd.constants import (
+    DEFAULT_TIMEGUARD_US, DEFAULT_VELOCITY_WINDOW, MAX_DOF,
+    VelocityEstimatorMode
+)
 from forcedimension.typing import (
     IntArray, SupportsPtr, SupportsPtrs3, c_double_ptr, c_int_ptr, c_ushort_ptr
 )
+
 
 class VersionTuple(NamedTuple):
     """
@@ -44,6 +52,202 @@ class UpdateOpts(NamedTuple):
     ft: Optional[float] = 4000
     req: Optional[float] = 4000
     gripper: Optional[GripperUpdateOpts] = None
+
+
+class TrajectoryGenParam(pydantic.BaseModel):
+    vmax: float = nan
+    amax: float = nan
+    jerk: float = nan
+
+    def __iter__(self):
+        yield self.vmax
+        yield self.amax
+        yield self.jerk
+
+    @pydantic.field_validator('vmax')
+    @classmethod
+    def validate_vmax(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError("vmax must be greater than 0")
+
+
+    @pydantic.field_validator('amax')
+    @classmethod
+    def validate_amax(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError("amax must be greater than 0")
+
+
+    @pydantic.field_validator('jerk')
+    @classmethod
+    def validate_jerk(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError("jerk must be greater than 0")
+
+
+class VelocityConfig(pydantic.BaseModel):
+    window: int = DEFAULT_VELOCITY_WINDOW
+    mode: VelocityEstimatorMode = VelocityEstimatorMode.WINDOWING
+
+    @pydantic.field_validator('window')
+    @classmethod
+    def validate_window(cls, val: Optional[int]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError("window must be greater than 0")
+
+
+class HapticDeviceInfo(pydantic.BaseModel):
+    serial_number: Optional[int] = None
+    has_base: bool
+    has_wrist: bool
+    has_active_wrist: bool
+    has_gripper: bool
+    has_active_gripper: bool
+    is_left_handed: bool
+
+    @pydantic.field_validator('serial_number')
+    @classmethod
+    def validate_serial_number(cls, val: Optional[int]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError("serial_number must be greater than 0")
+
+
+class HapticDeviceConfig(pydantic.BaseModel):
+    class GripperConfig(pydantic.BaseModel):
+        max_force: Optional[float] = None
+        velocity_estimator: VelocityConfig = dataclasses.field(
+            default_factory=VelocityConfig
+        )
+
+        @pydantic.field_validator('max_force')
+        @classmethod
+        def validate_max_force(cls, val: Optional[float]):
+            if val is None:
+                return
+
+            if val < 0:
+                raise ValueError(
+                    "max_force must be None or at least 0 [N]"
+                )
+
+    class RegulatorConfig(pydantic.BaseModel):
+        is_pos_regulated: bool = False
+        is_rot_regulated: bool = False
+        is_grip_regulated: bool = False
+        motor_ratio_max: float = 1.0
+        enc_move_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        enc_track_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        pos_move_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        pos_track_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        rot_move_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        rot_track_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        grip_move_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+        grip_track_param: TrajectoryGenParam = dataclasses.field(
+            default_factory=TrajectoryGenParam
+        )
+
+        @pydantic.field_validator('motor_ratio_max')
+        @classmethod
+        def validate_motor_ratio_max(cls, val: float):
+            if val < 0.0 or val > 1.0:
+                raise ValueError("motor_ratio_max must be between 0.0 and 1.0")
+
+    is_simulator_enabled: bool = False
+    is_force_enabled: bool = True
+    is_brake_enabled: bool = False
+    is_button_emulation_enabled: bool = True
+    is_gravity_compensation_enabled: bool = True
+    com_mode: Literal['async', 'sync', 'virtual', 'network'] = 'async'
+    timeguard: int = DEFAULT_TIMEGUARD_US
+    linear_velocity_estimator: VelocityConfig = dataclasses.field(
+        default_factory=VelocityConfig
+    )
+    angular_velocity_estimator: VelocityConfig = dataclasses.field(
+        default_factory=VelocityConfig
+    )
+    max_force: Optional[float] = None
+    max_torque: Optional[float] = None
+    standard_gravity: float = 9.81
+
+    gripper: Optional[GripperConfig] = dataclasses.field(
+        default_factory=GripperConfig
+    )
+
+    regulator: Optional[RegulatorConfig] = dataclasses.field(
+        default_factory=RegulatorConfig
+    )
+
+    @pydantic.field_validator('timeguard')
+    @classmethod
+    def validate_timeguard(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0 and val != -1:
+            raise ValueError(
+                "timeguard must be None, at least 0 us, or -1 "
+                "(which sets it to its default value)."
+            )
+
+    @pydantic.field_validator('max_force')
+    @classmethod
+    def validate_max_force(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError(
+                "max_force must be None or at least 0 [N]"
+            )
+
+    @pydantic.field_validator('max_torque')
+    @classmethod
+    def validate_max_torque(cls, val: Optional[float]):
+        if val is None:
+            return
+
+        if val < 0:
+            raise ValueError(
+                "max_torque must be None or at least 0 [Nm]"
+            )
+
+    @pydantic.field_validator('standard_gravity')
+    @classmethod
+    def validate_standard_gravity(cls, val: float):
+
+        if val < 0:
+            raise ValueError(
+                "standard_gravity must be at least 0 [m/s^2]"
+            )
 
 
 class Vector3(array, SupportsPtrs3[c_double], SupportsPtr[c_double_ptr]):
@@ -343,6 +547,7 @@ DefaultMat6x6Type = Mat6x6
 try:
     import numpy as np
     import numpy.typing as npt
+
     from forcedimension.typing import FloatArray
 
     class NumpyVector3(
@@ -362,7 +567,6 @@ try:
                 raise ValueError
 
             return arr
-
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -451,7 +655,6 @@ try:
                 raise ValueError
 
             return arr
-
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
